ff8aa21802534eb964c74f9801c27a271cef641b
Rajan Hari Ambrish <69025974+RajanHari@users.noreply.github.com>
2020-08-01 21:37:02 +0530

    Update speechrecog2.py

--- speechrecog2.py
+++ speechrecog2.py
@@ -8,7 +8,7 @@ speech_to_text = SpeechToTextV1(
     url = "YOUR_URL"
 )
 with speech as source:
-    print("say something!!…")
+    print("Please speak now")
     audio_file = r.adjust_for_ambient_noise(source)
     audio_file = r.listen(source)
 speech_recognition_results = speech_to_text.recognize(audio=audio_file.get_wav_data(), content_type='audio/wav').get_result()
